93c51c09fea1b974674fa92ba70a69bad34f5b9a
Connel Hooley <connelhooley@gmail.com>
2020-06-07 16:12:16 +0100

    Added missing tests for role prop

--- src/Authorized.test.tsx
+++ src/Authorized.test.tsx
@@ -11,7 +11,7 @@ const useUserMock = useUser as jest.MockedFunction<typeof useUser>;
 
 describe("Authorized", () => {
     test("should render children when user is signed in", async () => {
-        // Arrange        
+        // Arrange
         useUserMock.mockImplementation(() => ({
             id: "id",
             token: "some token",
@@ -30,8 +30,48 @@ describe("Authorized", () => {
         expect(await screen.findByTestId("assert")).toHaveTextContent(content);
     });
 
+    test("should render children when user is signed in and the given role does match", async () => {
+        // Arrange
+        const role = "some role";
+        useUserMock.mockImplementation(() => ({
+            id: "id",
+            token: "some token",
+            name: "some name",
+            email: "some@email.com",
+            role,
+        }));
+        const content = "hello, world";
+
+        // Act
+        render(
+            <Authorized role={role}><p data-testid="assert">{content}</p></Authorized>
+        );
+        
+        // Assert
+        expect(await screen.findByTestId("assert")).toHaveTextContent(content);
+    });
+
+    test("should not render children when user is signed in but the given role doesn't match", () => {
+        // Arrange
+        useUserMock.mockImplementation(() => ({
+            id: "id",
+            token: "some token",
+            name: "some name",
+            email: "some@email.com",
+            role: "some role",
+        }));
+
+        // Act
+        render(
+            <Authorized role="some other role"><p data-testid="assert">hello, world</p></Authorized>
+        );
+        
+        // Assert
+        expect(screen.queryByTestId("assert")).toBeNull();
+    });
+
     test("should not render children when user is signed out", () => {
-        // Arrange        
+        // Arrange
         useUserMock.mockImplementation(() => false);
 
         // Act
@@ -44,7 +84,7 @@ describe("Authorized", () => {
     });
 
     test("should not render children when user is signing in or out", () => {
-        // Arrange        
+        // Arrange
         useUserMock.mockImplementation(() => undefined);
 
         // Act
