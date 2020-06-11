4f4defc4b36fb33c0c432663442e890a39f5c74d
Connel Hooley <connelhooley@gmail.com>
2020-06-11 20:56:07 +0100

    removed e2e tests

--- e2e/run.ps1
+++
@@ -1,9 +0,0 @@
-Push-Location ".."
-npm run build
-npm link
-Pop-Location
-
-npm link "@connelhooley/react-oidc"
-docker-compose up --build --detach
-dotnet test "./Tests/Tests.csproj"
-docker-compose --file "docker-compose.yml" down
